d8a3ae6ba4449c43e655a3f0966e83c0e4e76b1f
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-04-15 18:53:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,9 +4,79 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  SYM m[size];
+  int count;
+  int head;
+  
+ public:
+  TPQueue() {
+    count = 0;
+    head = 0;
+    for (int i = 0; i < size; i++) {
+      m[i].prior = 0;
+    }
+	}
+  void push(T elm) {
+    int pos = 0;
+    if (count == size) {
+      cout << Count() << endl;
+			return;
+		}
+    if (count == 0 && head == 0) {
+      pos = 0;
+      m[0] = elm;
+      count++;
+    } else {
+      for (int i = 0; i <= count; i++) {
+        pos = head + i;
+        if (pos > size - 1)
+          pos = pos - size;
+        if (m[pos].prior < elm.prior) {
+          UpHeaval(pos);
+          m[pos] = elm;
+          count++;
+          break;
+				}
+			}
+		}
+		return;
+	}
+  T pop() {
+    if (count == 0) {
+      cout << Count() << endl;
+      return SYM{'0',0};
+		}
+    SYM Buf = m[head];
+    m[head].ch = 0;
+    m[head].prior = 0;
+    head++;
+    if (head > size-1)
+      head = 0;
+		count--;
+    if (count == 0)
+      head = 0;
+    return Buf;
+	}
+  void UpHeaval(int pos) {
+		int end = head + count;
+		if (end > size - 1)
+      end = end - size;
+    int num = head + count - pos;
+    if (num >= size)
+      num = num - size;
+    for (int i = 0; i < num; i++) {
+			if (end == 0) {
+        m[0] = m[size - 1];
+        end = size-1;
+      } else {
+        m[end] = m[end - 1];
+        end--;
+			}
+		}
+		return;
+	}
 };
-
 struct SYM {
   char ch;
   int prior;
